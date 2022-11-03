dd22b6a0b43ec6a0fbc4e45270d111c7b8170399
Volodymyr <volodymyr.skalskyi@uipath.com>
2022-11-03 17:47:26 +0200

    basic line chart

--- app.js
+++ app.js
@@ -1,3 +1,75 @@
-d3.csv('https://www.marketwatch.com/investing/stock/path/downloaddatapartial?startdate=01/03/2022 00:00:00&enddate=11/02/2022 23:59:59&daterange=d30&frequency=p1d&csvdownload=true&downloadpartial=false&newdates=false').then((data) => {
+const type = (d) => ({ date: d.Date, price: +d.Close });
+
+const ready = (data) => {
+    const sorted = data.sort((a, b) => d3.ascending(a.date, b.date));
+    console.log('sorted => ', sorted);
+
+    const margin = { top: 40, left: 40, bottom: 40, right: 40 };
+
+    const width = 800 - margin.left - margin.right;
+    const height = 500 - margin.top - margin.bottom;
+
+    const xExtent = d3.extent(sorted, d => new Date(d.date));
+
+    const xScale = d3.scaleLinear()
+        .domain(xExtent)
+        .range([0, width]);
+
+    const yExtent = d3.extent(sorted, d => d.price);
+
+    const yScale = d3.scaleLinear()
+        .domain(yExtent)
+        .range([height, 0]);
+
+    const svg = d3.select('.chart')
+        .append('svg')
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom)
+        .append('g')
+        .attr('transform', `translate(${margin.left}, ${margin.top})`)
+
+
+    const xAxis = d3.axisBottom(xScale)
+        .tickFormat(x => {
+            const date = new Date(x);
+            return `${date.getDate() + 1}/${date.getMonth() + 1}/${date.getFullYear()}`
+        })
+        .ticks(5)
+        .tickSizeOuter(0);
+
+    const xAxisDraw = svg
+        .append('g')
+        .attr('class', 'x-axis')
+        .attr('transform', `translate(0, ${height})`)
+        .call(xAxis);
+
+    const yAxis = d3.axisLeft(yScale)
+        .ticks(7)
+        .tickFormat(y => `${y}$`)
+        .tickSizeOuter(0)
+        .tickSizeInner(-width);
+
+    const yAxisDraw = svg
+        .attr('class', 'y-axis')
+        .append('g')
+        .call(yAxis);
+
+    const lineGen = d3.line().x(d => xScale(new Date(d.date))).y(d => yScale(d.price));
+
+    const chartGroup = svg.append('g').attr('class', 'line-chart');
+
+    chartGroup
+        .selectAll('.line-series')
+        .data(sorted)
+        .enter()
+        .append('path')
+        .attr('class', d => `line-series-${d.date}`)
+        .attr('d', () => lineGen(sorted))
+        .style('fill', 'none')
+        .style('stroke', '#ff8a65');
+};
+
+d3.csv('https://www.marketwatch.com/investing/stock/path/downloaddatapartial?startdate=01/03/2022 00:00:00&enddate=11/02/2022 23:59:59&daterange=d30&frequency=p1d&csvdownload=true&downloadpartial=false&newdates=false', type).then((data) => {
     console.log('data ->', data);
+    ready(data);
 });

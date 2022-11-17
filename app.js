2bc04d3c967ec1e38abc76b805422d085f89c660
Volodymyr Skalskyi <volodymyr.skalskyi@uipath.com>
2022-11-17 13:42:14 +0200

    data for heatmap

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
+const margin = { top: 40, left: 40, bottom: 40, right: 40 };
+
+const width = 800 - margin.left - margin.right;
+const height = 500 - margin.top - margin.bottom;
+
+const formatDate = (x) => {
+    const date = new Date(x);
+    return `${date.getDate() + 1}/${date.getMonth() + 1}/${date.getFullYear()}`
+};
+
 const renderLineChart = (data) => {
-    const formatDate = (x) => {
-        const date = new Date(x);
-        return `${date.getDate() + 1}/${date.getMonth() + 1}/${date.getFullYear()}`
-    };
-
     const sorted = data.sort((a, b) => d3.ascending(a.date, b.date));
-    const margin = { top: 40, left: 40, bottom: 40, right: 40 };
-
-    const width = 800 - margin.left - margin.right;
-    const height = 500 - margin.top - margin.bottom;
 
     // container
     const svg = d3.select('.chart')
@@ -139,9 +140,44 @@ const renderLineChart = (data) => {
         });
 };
 
+const renderHeatmapChart = (data) => {
+    const transformData = (input) => {
+        const groups = d3.groups(input, item => item.month);
+
+        return d3.map(groups, (group) => {
+            return {
+                month: group[0],
+                avgPrice: d3.median(group[1], i => i.price),
+                avgVolume: d3.median(group[1], i => i.volume),
+            };
+        });
+    };
+
+    const chartData = transformData(data);
+
+    console.log('chartData => ', chartData);
+
+    // render container
+    const renderSvg = () => d3.select('.heatmap')
+        .append('svg')
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom)
+        .append('g')
+        .attr('transform', `translate(${margin.left}, ${margin.top})`)
+
+    const svg = renderSvg();
+};
 
 
-d3.csv('https://www.marketwatch.com/investing/stock/path/downloaddatapartial?startdate=01/03/2022 00:00:00&enddate=11/02/2022 23:59:59&daterange=d30&frequency=p1d&csvdownload=true&downloadpartial=false&newdates=false', (d) => ({ date: new Date(d.Date).getTime(), price: +d.Close })).then((data) => {
-    console.log('data ->', data);
-    renderLineChart(data);
-});
+d3.csv('https://www.marketwatch.com/investing/stock/path/downloaddatapartial?startdate=01/03/2022 00:00:00&enddate=11/02/2022 23:59:59&daterange=d30&frequency=p1d&csvdownload=true&downloadpartial=false&newdates=false',
+    (d) => ({
+        date: new Date(d.Date).getTime(),
+        price: +d.Close,
+        volume: +d.Volume.replaceAll(',', ''),
+        month: new Date(d.Date).getMonth() + 1,
+    }))
+    .then((data) => {
+        console.log('data ->', data);
+        renderLineChart(data);
+        renderHeatmapChart(data);
+    });
